b0ea53aa35c818c15a7b639b91f446ea03ee02a4
freeanser <bnn01615@gmail.com>
2024-04-11 16:56:46 -0700

    Add API endpoint and model prediction functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import numpy as np  # 添加此行以转换数据点为数组
 import joblib
 
 app = Flask(__name__)
@@ -11,8 +12,10 @@ model = joblib.load('trained_model.joblib')
 def predict():
     # 從請求中獲取資料
     data = request.get_json()
+    # 將資料轉換為 NumPy 數組
+    data_array = np.array(data['features'])  # 假設 'features' 是要預測的特徵
     # 使用模型進行預測
-    prediction = model.predict(data)
+    prediction = model.predict(data_array.reshape(1, -1))  # 這裡假設您的模型僅接受一個樣本，所以使用 reshape 進行轉換
     # 回傳預測結果
     return jsonify({'prediction': prediction.tolist()})
 
