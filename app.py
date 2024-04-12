825fc94197f89ecae64f308fa72c5a8bfe7f6b2d
freeanser <bnn01615@gmail.com>
2024-04-11 17:22:54 -0700

    add app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-import numpy as np  # 添加此行以转换数据点为数组
+from flask import Flask, request, jsonify, render_template
+import numpy as np
 import joblib
 
 app = Flask(__name__)
@@ -7,16 +7,23 @@ app = Flask(__name__)
 # 載入模型
 model = joblib.load('trained_model.joblib')
 
-# 定義預測端點
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # 從請求中獲取資料
     data = request.get_json()
-    # 將資料轉換為 NumPy 數組
-    data_array = np.array(data['features'])  # 假設 'features' 是要預測的特徵
-    # 使用模型進行預測
-    prediction = model.predict(data_array.reshape(1, -1))  # 這裡假設您的模型僅接受一個樣本，所以使用 reshape 進行轉換
-    # 回傳預測結果
+    # 提取特征并转换为NumPy数组
+    features = [
+        data['Dew_Point_Temp_C'],
+        data['Press_kPa'],
+        data['Rel_Hum_%'],
+        data['Wind_Speed_km/h']
+    ]
+    data_array = np.array(features).reshape(1, -1)
+    # 使用模型进行预测
+    prediction = model.predict(data_array)
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
